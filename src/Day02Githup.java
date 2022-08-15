fd8a9f7c8e15a4ebc078abf51285e70941dc98bd
Hanım Acar <filozofbilir43@gmail.com>
2022-08-15 20:01:26 +0200

    yenimesaj

---
+++ src/Day02Githup.java
@@ -0,0 +1,18 @@
+public class Day02Githup {
+    /*
+    1-) git init--->Local repo olusturmak ıcın yanı .git ile klasorumuzun ııcndekı dosyaları
+    ılıskılendırmek ıcın kullanılır
+    2-)git add .  -->Workıng space'den(yanı yerel) dosyalarımı staging area'ya (yanı commıtlemek
+     ıcın bekelenen yer) gonderır
+    3-)git commit -m"mesaj" -->Staging area 'dan commıt stora dosyalarımı gondermek ıcın kullanılır
+    (commıt yanı versıyon yanı surum olusturmus olurum)
+    4-)git push --> Uzak repo'ya(Yani remote - Githup) gondermek ıcın kullandıgımız kod
+    Yalnız git push komutunu dırek kullanmak ıstersek bir kez
+    git remote add origin adress
+    git push -u origin master
+
+  Not:Yukarıdakı bu iki komutu tek seferde kullandıktan  sonra ikinci commit'lerim icin sadece git push
+  kullanırız
+     */
+
+}
