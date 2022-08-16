80fff4cbd6b6203dd833ba126eb57ecbd5a24c80
HanimAcar <87536582+HanimAcar@users.noreply.github.com>
2022-08-16 21:08:39 +0200

    Update Day01Git.java

--- src/Day01Git.java
+++ src/Day01Git.java
@@ -4,5 +4,7 @@ public class Day01Git {
         System.out.println("Git 2. Deneme");
         System.out.println("Git 3. Deneme");
         System.out.println("Git 4. Deneme");
+           System.out.println("Git 5. Deneme");
+        
     }
 }
